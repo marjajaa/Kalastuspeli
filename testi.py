e9f7de63c91d75f43f3c03779ab2c5b72380d1f8
marjajaa <marja.k.jaatinen@gmail.com>
2026-04-01 09:38:31 +0300

    testi päävalikosta

--- testi.py
+++ testi.py
@@ -1,1 +1,35 @@
-print("kakapyly")
+ #kalalajit = ["kuha", "ahven", "taimen", "lohi", "hauki", "siika"]
+
+
+def paavalikko():
+
+    while True:
+        print("kakka")
+        print("Päävalikko")
+        print("Valitse 1, jos haluat aloittaa kalastamisen")
+        print("Valitse 2, jos haluat poistua pelistä")
+
+        valinta = input("Valitse 1 tai 2: ")
+        print(f"debug {valinta}")
+
+        if valinta == "1":
+            print("Aloitit pelin")
+            
+                
+
+        elif valinta == "2":
+            break
+
+        else:
+            print("Syötä numero 1 tai 2!")
+
+    print(f"debug {valinta}")
+
+
+paavalikko()
+
+
+
+
+
+
